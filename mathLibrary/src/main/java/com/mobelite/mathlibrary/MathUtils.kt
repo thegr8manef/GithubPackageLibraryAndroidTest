e67b5b84c2cef9c0ad07ed80ca5a5cf05a7379a5
thegr8manef <jedidimanef@gmail.com>
2024-03-27 12:49:53 +0100

    first commit

---
+++ mathLibrary/src/main/java/com/mobelite/mathlibrary/MathUtils.kt
@@ -0,0 +1,10 @@
+package com.mobelite.mathlibrary
+
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+data class Point(val x: Double, val y: Double)
+
+fun Point.calculateDistanceTo(point: Point): Double {
+    return sqrt((x - point.x).pow(2) + (y - point.y).pow(2))
+}
